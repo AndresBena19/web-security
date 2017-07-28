0c9d5017ffa87e0888c3674d25d20b195940e7c4
AndresBena19 <andres_felipe16@hotmail.es>
2017-07-28 17:15:55 -0400

    Details

---
+++ Getting Details/Getinfo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import os
+import socket
+import shutil
+import errno
+if os.name != "nt":
+    import fcntl
+import struct
+import uuid
+import argparse
+
+def IP(inter):
+ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ IP_ADDRESS = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,
+ struct.pack('256s', inter[:15]))[20:24])
+ return IP_ADDRESS
+
+def MAC(inter):
+ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ info = fcntl.ioctl(s.fileno(), 0x8927, struct.pack('256s', inter[:15]))
+ MAC_ADDRESS = ''.join(['%02x:' % ord(char) for char in info[18:24]]) [:-1]
+ return MAC_ADDRESS
+
+
+
+
+if __name__=="__main__":
+ parser = argparse.ArgumentParser(description="Script extraer la IP y MAC")
+ parser.add_argument('-I', help="Interfaz", required=True, dest='INT')
+ args = parser.parse_args()
+
+ I = IP(args.INT)
+ M = MAC(args.INT)
+
+
+
+print "The  interfaz " + args.INT +" have a IP:"+ I +" And MAC:" + M  
+
+
+
+
+
+
