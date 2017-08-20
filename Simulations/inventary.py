3022d204669d669a6bae8d4690ba426a335489dc
AndresBena19 <andres_felipe16@hotmail.es>
2017-08-19 23:37:11 -0500

    Update inventary.py
    End

--- Simulations/inventary.py
+++ Simulations/inventary.py
@@ -7,9 +7,8 @@ def Inventary(di):
  s=25
  Li=[200]
  Oi=[]
- 
- while(i!=101):
-  
+
+ while(i!=101): 
   i=i+1 
   if(Li[i-1]< s):
       Oi.insert(i-1,S-Li[i-1])
@@ -38,7 +37,7 @@ def Promedio(Lista,N):
 def AfterReview(Li,Oi):
   L=[]
   i=0
-  while(i!=len(Li)):
+  while(i!=101):
    i=i+1
    L.insert(i-1, Li[i-1]+Oi[i-1])
   return L
@@ -47,7 +46,7 @@ def AfterReview(Li,Oi):
 
 if __name__ == "__main__":
 
- di=[0,20,45,20,30,45,25,45,40,30,40,30,40,15,40,20,40,30,45,40,15,15,30,25,30,25,45,40,35,25,40,20,15,30,45,30,35,35,20,40,30,45,20,40,35,45,30,20,30,45,35,45,25,25,40,25,35,25,15,35,45,40,20,30,30,25,30,35,15,20,35,20,15,25,45,20,20,15,15,15,15,40,20,40,25,20,35,40,25,15,45,40,20,20,15,15,45,35,40,20,30,0]
+ di=[0,20,45,20,30,45,25,45,40,30,40,30,40,15,40,20,40,30,45,40,15,15,30,25,30,25,45,40,35,25,40,20,15,30,45,30,35,35,20,40,30,45,20,40,35,45,30,20,30,45,35,45,25,25,40,25,35,25,15,35,45,40,20,30,30,25,30,35,15,20,35,20,15,25,45,20,20,15,15,15,15,40,20,40,25,20,35,40,25,15,45,40,20,20,15,15,45,35,40,20,30,0] 
  Li=[]
  Oi=[]
  L=[]
@@ -75,32 +74,31 @@ if __name__ == "__main__":
  print "************************************************************"
  print "d = o = " + str(int(Acum)) + "/100 = " + str(SO[0])+ " items per time interval"
  print "************************************************************"
- print "Average of sorted items (L ) = " + str(SL[0])
+ print "Average of sorted items (Li) = " + str(SL[0])
  print "************************************************************"
  print "Operating cost"
+ print " "
 
  U=0.0
  for x in range(0, 101):
-    if(Oi[x]!=0):
-     U=U+1
+    if(Oi[x]>0):
+     U=U+1 
  Up=U/100
- print "Average setup cost  (u ) :" + str(Up)
+ print "Average setup cost  (u) :" + str(Up)
 
  p=0
- while(p!=101):
+ while(p!=100):
    p=p+1
-   Lmas.insert(p-1, ((L[p-1])**2)/2*di[p])
-
- k=0
- while(k!=101):
-   k=k+1
-   Lmenos.insert(k-1,(di[k]-L[k-1])**2/2*di[k])
+   if(di[p]<=L[p-1]):
+     Lmas.insert(p-1, L[p-1]-0.5*di[p])
+   else:
+     Lmas.insert(p-1, (L[p-1]*L[p-1])/(2.0*di[p]))
+     Lmenos.insert(p-1,((di[p]-L[p-1])*(di[p]-L[p-1]))/(2.0*di[p]))
 
  SLmas = Promedio(Lmas,100)
+ 
  SLmenos = Promedio(Lmenos,100)
-
-
- print "Average holding level (L+) :" + str(SLmas[0])
+ print "Average holding level (L+) :" +  str(SLmas[0])
  print "Average shortage level (L-) :" + str(SLmenos[0])
  print "Time-average inventory(L) :" + str(SLmas[0]-SLmenos[0]) 
 
@@ -110,9 +108,9 @@ if __name__ == "__main__":
  C= 500*SLmas[0]
  D= 3000*SLmenos[0]
  print "***----****----****----*****----******----****---*****"
- print "item cost:"+ str(A) + " per week"
- print "setup cost:"+ str(B) + " per week"
- print "holding cost:"+ str(C) + " per week"
- print "shortage cost:" +str(D) + " per week"
- 
- print "Average dependent cost:" + str(A+B+C+D) 
+ print "item cost:"+ str(int(A)) + " per week"
+ print "setup cost:"+ str(int(B)) + " per week"
+ print "holding cost:"+ str(int(C)) + " per week"
+ print "shortage cost:" +str(int(D)) + " per week"
+ print " "
+ print "Average dependent cost:" + str(int(A+B+C+D)) 
