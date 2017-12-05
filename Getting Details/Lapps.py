d0aa7c738f4da99a7105a3277f406e47831ec764
AndresBena19 <andres_felipe16@hotmail.es>
2017-12-05 15:37:20 -0500

    LinuxApps

--- Getting Details/Lapps.py
+++ Getting Details/Lapps.py
@@ -1,27 +1,48 @@
 import subprocess
+from subprocess import Popen, PIPE
+import sys
 
-if __name__ == '__main__':
 
-    #This is the place where distribution is save
-    outfd = open('Sl', 'w+')
+def check_execs(*progs):
+    #We recieve a tupla for future funtional options
+    for prog in progs:
+        try:
+            Popen([prog, '--help'], stdout=PIPE, stderr=PIPE)
+        except OSError:
+            msg = 'The {0} program is necessary to run this script'.format(prog)
+            sys.exit(msg)
+        else:
+            Get_apps()
 
-    #Getting the distributions
-    subprocess.call(['uname', '-a'], stdout=outfd)
-    outfd.close()
 
-    #read the file with distribution
-    fd = open('Sl', 'r')
-    output = fd.read()
-
-    #Apps on the host
+def Get_apps():
+    #Finding the distributions type
+    Host_info=subprocess.check_output(['uname -a'], shell=True)
+    packets = open('Packets.txt', 'w+')
+    temp = open('temp.txt', 'w+')
     apps = open('Apps.txt', 'w+')
 
+
     #getting the apps of the system
-    if(output.find("Debian")!= -1):
-        subprocess.call(['dpkg', '--get-selections'], stdout=apps)
+    if(Host_info.find("Debian")!= -1):
+        subprocess.call(['dpkg', '-l'], stdout=temp)
+        subprocess.call(['dpkg', '--get-selections'], stdout=packets)
+
+        subprocess.Popen(('cut', '-c','1-65', 'temp.txt'), stdout=apps)
+        subprocess.call(['rm', 'temp.txt'])
+
+
+
+
+
+
     else:
-        if(output.find("fedora")!= -1 ):
+        if(Host_info.find("fedora")!= -1 ):
             subprocess.call(['rmp', '-qa'])
 
-    #Delete de file with the distribution
-    subprocess.call(['rm', 'Sl'])
+
+
+
+if __name__ == '__main__':
+    #Checking
+    check_execs('dpkg')
